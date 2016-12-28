a57ae132797eea439ddddcd1762c85e84cd49640
Roc-Chen <444873494@qq.com>
2016-12-28 11:46:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# vqweb
+VQ运营中心
+
+### 使用说明
+
+# 安装依赖
+npm install
+
+# 运行服务
+npm run dev
+
+# 生成发布
+npm run build
---
+++ src/components/charts/Page7.vue
@@ -0,0 +1,31 @@
+<template>
+	<el-row>
+		<el-col :span="12">
+			<div class="block">
+			  <span class="demonstration">默认不区分颜色</span>
+			  <el-rate v-model="value1"></el-rate>
+			</div>
+		</el-col>
+		<el-col :span="12">
+			<div class="block">
+			  <span class="demonstration">区分颜色</span>
+			  <el-rate
+			    v-model="value2"
+			    :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+			  </el-rate>
+			</div>
+		</el-col>
+	</el-row>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value1: null,
+        value2: null
+      }
+    }
+  }
+</script>
